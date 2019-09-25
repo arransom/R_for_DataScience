172fe92713c7533b394aa44c71125742cdd68d74
Amy <arransom@gmail.com>
2019-09-24 22:53:06 -0700

    - Completed exercise for 3.5 - Facets

--- ch3_data-visualization.R
+++ ch3_data-visualization.R
@@ -94,3 +94,95 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, color = displ <5))
 
 # Answer: it maps the aesthetic to the data using the criteria you have given
+
+
+# 3.5 - Facets -----------------------------------------------------------------
+
+# What happens if you facet on a continuous variable?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ cty, nrow = 4)
+# Answer: You get a facet for every one of the distinct values in the 
+#         continuous variable
+
+# What do the empty cells in plot with facet_grid(drv ~ cyl) mean? 
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(drv ~ cyl)
+# Answer: There were no observations in the dataset with those combinations 
+#         of drive and cyl
+
+# How do they relate to this plot?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = drv, y = cyl))
+
+# Answer: That plot shows the possible combinations of drv and cyl and confirms
+#         that no cars within the dataset match that combination of drv and cyl
+
+# What plots does the following code make? What does . do?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
+
+ggplot(data = mpg) +  
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ cyl)
+
+#Answer: The first block of code creates a set of plots showing the
+#        relationship between displ, hwy, and drv.
+#        displ and hwy are displayed on the x and y axes, respectively,
+#        Each row shows a different value of drv.  The . stands in for a
+#        column variable and indicates that there is not one in this facet_grid. 
+#
+#        The second block of code creates a set of plots showing the
+#        relationship between displ and hwy for each category of cyl 
+#        Each column shows a different value of cyl.  The . stands in for a
+#        row variable and indicates that there is not one in this facet_grid. 
+
+
+# Take the first faceted plot in this section:
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))  
+
+
+# What are the advantages to using faceting instead of the colour aesthetic? 
+# Answer: It's much easier to see the differences between the vehicle types 
+#         when they are separated into individual plots. Looking at the plots 
+#         side to side, with the same axis ranges, you may be able to make new 
+#         analytical insights
+
+# What are the disadvantages? 
+# Answer: If there are only a few observations in each category, or only a few 
+#         categories to facet by, faceting may not be necessary or make the 
+#         information easier to read.
+
+# How might the balance change if you had a larger dataset?
+# Answer: A single plot may be even more visually "busy" or overwhelming with  
+#         a larger dataset. 
+
+# Read ?facet_wrap. What does nrow do? What does ncol do? 
+?facet_wrap
+# Answer: nrow specifies the number of rows; ncol specifies the number of 
+#         columns. 
+
+
+# What other options control the layout of the individual panels? 
+# Answer: scales, shrink, labeller, and switch
+
+# Why doesn't facet_grid() have nrow and ncol arguments?
+# Answer: The number of rows and columns will be informed by the number of
+#         unique values or levels in the row variable and column variable, 
+#         respectively
+
+# When using facet_grid() you should usually put the variable with more 
+# unique levels in the columns. Why?
+# Answer: It will be a more efficient use of screen space
+
+
+
