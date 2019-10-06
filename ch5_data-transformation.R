c78fcd2fa07b1c79e27c287160718a48bdc877d6
Amy <arransom@gmail.com>
2019-10-06 16:21:48 -0700

    Complete exercises for 5.6 - Grouped summaries with summarize()

--- ch5_data-transformation.R
+++ ch5_data-transformation.R
@@ -160,7 +160,7 @@ select(flights, contains("TIME"))
 # ignore.case = FALSE
 
 
-# 5.5 Add new variables with Mutate() ------------------------------------------
+# 5.5 Add new variables with mutate() ------------------------------------------
 
 # Currently dep_time and sched_dep_time are convenient to look at, but hard to 
 # compute with because they’re not really continuous numbers. 
@@ -170,10 +170,10 @@ select(flights, contains("TIME"))
 flight_times<-flights %>%
   mutate(dep_hour = dep_time %/% 100,
          dep_minute = dep_time %% 100,
-         dep_mins_past_midnight = dep_hour * 60 + dep_minute,
+         dep_min_past_midnight = dep_hour * 60 + dep_minute,
          sched_dep_hour = sched_dep_time %/% 100,
          sched_dep_minute = sched_dep_time %% 100,
-         sched_dep_mins_past_midnight = sched_dep_hour * 60 + sched_dep_minute) %>%
+         sched_dep_min_past_mid = sched_dep_hour * 60 + sched_dep_minute) %>%
   select(-c(dep_hour, dep_minute, sched_dep_hour, sched_dep_minute))
 
 
@@ -223,3 +223,142 @@ flight_delays<-flights %>%
 # What trigonometric functions does R provide?
 # Answer: R provides the following trigonometric functions: cos(), sin(), tan(), 
 # acos(), asin(), atan(), atan2(), cospi(), sinpi(), tanpi() 
+
+
+# 5.6 Grouped summaries with summarize() ---------------------------------------
+
+# Brainstorm at least 5 different ways to assess the typical delay 
+# characteristics of a group of flights. 
+
+# Narrow the dataset to exclude cancelled flights
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+# Mean arrival delay, mean arrival delay among flights with a positive delay,
+# Median arrival delay, median arrival delay among flights with a positive delay
+delay_averages <- not_cancelled%>%
+  group_by(dest) %>%
+  summarize(
+    mean_arr_delay = mean(arr_delay),
+    mean_pos_arr_delay = mean(arr_delay[arr_delay>0]),
+    median_arr_delay = median(arr_delay),
+    median_pos_arr_delay = median(arr_delay[arr_delay>0])
+    )
+
+# Percent of flights that arrive late(any amount), that arrive more than 
+# 30 minutes late, and that arrive more than 60 minutes late
+lateness_percentages <- not_cancelled%>%
+  group_by(dest) %>%
+  summarize(
+    pct_late = mean(arr_delay > 0),
+    pct_more_than_30min_late = mean(arr_delay > 30),
+    pct_more_than_1hr_late = mean(arr_delay > 60)
+    )
+
+# Mean delay among flights that departed late, that departed 30 to 60 minutes late, 
+# and that departed more than 60 minutes late
+delays_of_late_deps <- not_cancelled%>%
+  group_by(dest) %>%
+  summarize(
+    arr_delay_late_deps = mean(arr_delay[dep_delay>0]),
+    arr_delay_late_deps_30to60 = mean(arr_delay[dep_delay >= 30 & dep_delay < 60]),
+    arr_delay_late_deps_60plus = mean(arr_delay[dep_delay > 60]),
+  )   
+
+# Which is more important: arrival delay or departure delay?
+# Answer:  As a customer, I would argue that arrival delay is more important.
+#          It doesn't really matter if the plane departs late, as long as
+#          it arrives on time. 
+
+
+#Come up with another approach that will give you the same output as 
+# not_cancelled %>% count(dest) #and 
+# not_cancelled %>% count(tailnum, wt = distance) #(without using count()).
+not_cancelled %>% 
+  group_by(dest) %>%
+  summarize(count = n())
+
+not_cancelled %>% 
+  group_by(tailnum) %>%
+  summarize(total_miles = sum(distance, na.rm = TRUE))
+
+# Our definition of cancelled flights (is.na(dep_delay) | is.na(arr_delay) )
+# is slightly suboptimal. Why? Which is the most important column?
+
+# Answer: The definition includes flights that have a value for arrival delay
+#         but a missing value for departure delay,(which would likely indicate 
+#         that the flight did, in fact, depart - since it must have departed in 
+#         order to arrive).  It also includes flights that have a value for 
+#         departure delay but a missing value for arrival delay.  This could 
+#         indicate missing data, or a flight that didn't arrive. (Perhaps due 
+#         to being rerouted, returning to the departure airport, or crashing?)
+#         Of the two, arr_delay is the most important column. 
+
+  
+# Look at the number of cancelled flights per day. Is there a pattern? 
+flights_cancellations <- flights %>%
+  group_by(year, month, day) %>%
+  summarize(scheduled_flights = n(),
+            not_cancelled = sum((!is.na(dep_delay) & !is.na(arr_delay))),
+            cancelled = sum((is.na(dep_delay) & is.na(arr_delay))))
+
+# Answer: To examine the number of cancelled flights per day, I used a slightly
+#         different definition - I defined 'cancelled' as flights that were
+#         had a missing value for departure delay AND had a missing value for 
+#         arrival delay. 
+
+#         Scrolling through the output, you can see that higher numbers of 
+#         delayed flights are "clumped" across consecutive days.  This may be 
+#         due to storms or bad weather.  
+
+
+# Is the proportion of cancelled flights related to the average delay?
+flights_cancel_delay <- flights %>%
+  group_by(year, month, day) %>%
+  summarize(scheduled_flights = n(),
+            not_cancelled = sum((!is.na(dep_delay) & !is.na(arr_delay))),
+            cancelled = sum((is.na(dep_delay) & is.na(arr_delay))),
+            proportion_cancelled = cancelled/scheduled_flights,
+            mean_arr_delay = mean(arr_delay, na.rm = TRUE))
+
+ggplot(flights_cancel_delay, aes(x = mean_arr_delay, y = proportion_cancelled)) + 
+  geom_point() + 
+  geom_smooth()
+
+#Answer: Yes.  The mean arrival delay and the proportion of flights that are
+#        cancelled are positively correlated. 
+
+
+# Which carrier has the worst delays? 
+flights_worst<-flights %>%
+group_by(carrier) %>%
+  summarize(scheduled_flights = n(),
+            cancelled = sum((is.na(dep_delay) & is.na(arr_delay))),
+            mean_arr_delay = mean(arr_delay, na.rm = TRUE)) 
+
+flights_worst %>% arrange(desc(mean_arr_delay)) %>% head(20)
+
+# Answer: F9 is the carrier with the highest mean delays. This does not
+#         take into account the number of flights per day. However, the airline
+#         685 flights scheduled - a fairly large sample size. 
+
+# Challenge: can you disentangle the effects of bad airports vs. bad carriers? 
+# Why/why not? (Hint: think about 
+# flights %>% group_by(carrier, dest) %>% summarise(n()))
+
+flights_worst2<-flights %>%
+  group_by(carrier, dest) %>%
+  summarize(scheduled_flights = n(),
+            cancelled = sum((is.na(dep_delay) & is.na(arr_delay))),
+            mean_arr_delay = mean(arr_delay, na.rm = TRUE)) 
+
+flights_worst2 %>% arrange(desc(mean_arr_delay)) %>% head(20)
+
+# Answer: When you disaggregate by destination, the carrier 'F9' loses its
+#         place as the airline with the highest mean delays.  
+
+# What does the sort argument to count() do. When might you use it?
+?count
+# Answer: "sort = TRUE" will cause the output to be sorted in descending order 
+#          of n.  You could use it to learn the group with the highest count 
+#          of something: for instance the highest number of cancelled flights. 
